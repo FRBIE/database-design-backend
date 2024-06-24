d28c1fdfdaca84ab30cca72f202a7ee1702f44f5
xrp <2537821837@qq.com>
2024-06-24 20:25:54 +0800

    feat:项目增删改查完成初版，待优化

--- src/main/java/cn/xrp/projectmanagementbackend/controller/ProjectController.java
+++ src/main/java/cn/xrp/projectmanagementbackend/controller/ProjectController.java
@@ -9,6 +9,7 @@ import cn.xrp.projectmanagementbackend.exception.BusinessException;
 import cn.xrp.projectmanagementbackend.model.Project;
 
 import cn.xrp.projectmanagementbackend.service.ProjectService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
@@ -32,8 +33,14 @@ public class ProjectController {
     public BaseResponse<List<Project>> getProjectList(){
         return ResultUtil.success(projectService.getProjectList());
     }
+    @PostMapping("/add")
+    public BaseResponse<Boolean> addProject(@RequestBody Project project){
+        //前端做参数校验，这里暂时不做
+        boolean res = projectService.save(project);
+        return ResultUtil.success(res);
+    }
     @PostMapping("/delete")
-    public BaseResponse<Boolean> deleteUser(@RequestBody JsonNode index){
+    public BaseResponse<Boolean> deleteProject(@RequestBody JsonNode index){
         long id = index.get("id").longValue();
         if(id < 0){
             throw new BusinessException(ErrorCode.PARAMS_ERROR);
@@ -41,5 +48,38 @@ public class ProjectController {
         boolean b =  projectService.removeById(id);
         return ResultUtil.success(b);
     }
+    @PostMapping("/edit")
+    public BaseResponse<Boolean> deleteProject(@RequestBody Project project){
+        int projectID = project.getProjectID();
+        if(projectID < 0){
+            throw new BusinessException(ErrorCode.PARAMS_ERROR);
+        }
+        boolean res = projectService.updateById(project);
+        return ResultUtil.success(res);
+    }
+    @PostMapping("/search")
+    public BaseResponse<List<Project>> searchProject(@RequestBody Project project){
+        LambdaQueryWrapper<Project> wrapper = new LambdaQueryWrapper<>();
+        if(project.getProjectID() != null && !project.getProjectID().equals("")){
+            wrapper.eq(Project::getProjectID,project.getProjectID());
+        }
+        if(project.getProjectName() != null && !project.getProjectName().equals("")){
+            wrapper.like(Project::getProjectName,project.getProjectName());
+        }
+        if(project.getStartDate() != null&& !project.getStartDate().equals("")){
+            wrapper.eq(Project::getStartDate,project.getStartDate());
+        }
+        if(project.getManagerID() != null && !project.getManagerID().equals("")){
+            wrapper.eq(Project::getManagerID,project.getManagerID());
+        }
+        if(project.getProjectLevel() != null&&!project.getProjectLevel().equals("")){
+            wrapper.eq(Project::getProjectLevel,project.getProjectLevel());
+        }
+        if(project.getBudget() != null&& !project.getBudget().equals("")){
+            wrapper.eq(Project::getBudget,project.getBudget());
+        }
 
+        List<Project> projectList = projectService.list(wrapper);
+        return ResultUtil.success(projectList);
+    }
 }
